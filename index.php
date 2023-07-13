9f3ae85e18e4f4fc5be060863fe048b64ace41ea
Allan Renderos <or16025@ues.edu.sv>
2023-07-13 08:57:59 -0600

    nuevo cambio despliegue

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <h1>Hola mundo desde ECS allan denilson orellana</h1>  
-    <p>cambios de despliegue nuevos ues23</p>  
+    <p>ya me funciona por fin xddd</p>  
 
 </body>
 </html>
